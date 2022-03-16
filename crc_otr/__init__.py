2f58939a1a040ba95502751e0ae72c6a619df031
uros_bojanic <bu190077d@student.etf.bg.ac.rs>
2022-03-16 16:05:38 +0100

    Final package

--- crc_otr/__init__.py
+++ crc_otr/__init__.py
@@ -1,3 +1,10 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Created By  : Uros Bojanic
+# Created Date: 2022/03/16
+# ---------------------------------------------------------------------------
+
 # __init__.py
 from .crc_otr import crc_check
 from .crc_otr import crc_encode
